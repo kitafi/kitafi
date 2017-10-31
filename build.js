008e74074ec8c332781395164d5f1be83dace566
Keshav Saharia <keshav@techlabeducation.com>
2017-10-30 21:40:39 -0500

    added gitignore

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var nunjucks = require('nunjucks');
+var chalk = require('chalk');
 var build = require('./build.json');
 
 fs.watch(__dirname + '/lesson', function(eventType, filename) {
