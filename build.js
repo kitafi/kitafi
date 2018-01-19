37b4b3d431b10e2b32f7c121ccb361be2a7d57b9
Keshav Saharia <keshav@keshav.is>
2018-01-19 15:37:34 -0600

    saving before running new build

--- build.js
+++ build.js
@@ -12,6 +12,45 @@ var partial = {};
 var build = require('./build.json');
 build.path = path.join(__dirname, '..', build.directory);
 
+// Cache the vocabulary words learned in each lesson
+build.before = [];
+build.after = [];
+build.lesson.forEach(function(lesson) {
+	build.before.push([]);
+	build.after.push([]);
+});
+
+function beforeMessage(lessonIndex, before) {
+	var messages = [];
+
+	for (var l = 0 ; l < lessonIndex ; l++) {
+		let after = build.after[l];
+		if (before.swahili != null) {
+			for (var a = 0 ; a < after.length ; a++) {
+				if (after[a].swahili == before.swahili) {
+					messages.push('Found in lesson "' + build.lesson[lessonIndex] + '"');
+				}
+			}
+		}
+		else if (before.concept != null) {
+
+		}
+	}
+
+	return messages;
+};
+function similarArray(a, b) {
+	if (! Array.isArray(a) || ! Array.isArray(b) || a.length != b.length) {
+		return false;
+	}
+	for (var i = 0 ; i < a.length ; i++) {
+		if (b.indexOf(a[i]) < 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
 // Caches all JSON data
 var data = {};
 
@@ -26,8 +65,17 @@ function success(message, label) {
 // Renders a given file with the given template
 function render(templateName, fileName) {
 	if (fileName == '.DS_Store') return;
+	var fileId = path.basename(fileName, '.json');
 
 	templateName = path.basename(templateName, '.html');
+
+	var lessonIndex = -1, isLesson = false, lessonBefore, lessonAfter;
+	if (templateName == 'lesson' && build.lesson.indexOf(fileId) >= 0) {
+		isLesson = true;
+		lessonIndex = build.lesson.indexOf(fileId);
+		lessonBefore = build.before[lessonIndex];
+		lessonAfter = build.after[lessonIndex];
+	}
 
 	// Read the file
 	fs.readFile(path.join(__dirname, templateName, fileName), 'utf8', function(readError, content) {
@@ -43,29 +91,99 @@ function render(templateName, fileName) {
 
 				// Check for UUIDs and immediately write back the formatted JSON
 				// with any new UUIDs inserted
-				if (templateName == 'lesson' && Array.isArray(json.lesson)) {
-					let missingFields = false;
+				if (templateName == 'lesson') {
+					let updateJSON = false;
+
+					if (json.title == null) {
+						json.title = '';
+						updateJSON = true;
+					}
+					if (json.description == null) {
+						json.description = '';
+						updateJSON = true;
+					}
+					if (json.unit == null) {
+						json.unit = 'uncategorized';
+						updateJSON = true;
+					}
+					if (! Array.isArray(json.lesson)) {
+						json.lesson = [];
+						updateJSON = true;
+					}
+					if (! Array.isArray(json.before)) {
+						json.before = [];
+						updateJSON = true;
+					}
+					if (! Array.isArray(json.after)) {
+						json.after = [];
+						updateJSON = true;
+					}
+					// if (! Array.isArray(json.vocabulary)) {
+					// 	json.vocabulary = [];
+					// 	updateJSON = true;
+					// }
 
 					json.lesson.forEach(function(lesson) {
 						if (lesson.instruction == null) {
 							lesson.instruction = '';
-							missingFields = true;
+							updateJSON = true;
 						}
 						if (lesson.english == null) {
 							lesson.english = '';
-							missingFields = true;
+							updateJSON = true;
 						}
 						if (lesson.swahili == null) {
 							lesson.swahili = '';
-							missingFields = true;
+							updateJSON = true;
 						}
 						if (! lesson.uuid) {
 							lesson.uuid = uuid();
-							missingFields = true;
+							updateJSON = true;
 						}
 					});
 
-					if (missingFields) {
+					lesson.before.forEach(function(before) {
+						let message = beforeMessage(lessonIndex, before);
+						if (message.length == 0) {
+							message = null;
+						}
+
+						if (message != null) {
+							if (before.message == null || ! similarArray(message, before.message)) {
+								before.message = message;
+								updateJSON = true;
+							}
+						}
+						else {
+							if (before.message != null) {
+								delete before.message;
+								updateJSON = true;
+							}
+						}
+					});
+
+					build.after[lessonIndex] = lesson.after;
+
+					// if (Array.isArray(json.vocabulary) && lessonIndex >= 0) {
+					// 	for (var v = 0 ; v < json.vocabulary.length ; v++) {
+					// 		let vocab = json.vocabulary[v];
+					// 		let foundVocab = false;
+                    //
+					// 		for (var l = 1 ; l < build.lesson.indexOf(fileId) * 2 ; l += 2) {
+					// 			if (build.vocabulary[l].indexOf(vocab) >= 0) {
+					// 				foundVocab = true;
+					// 				break;
+					// 			}
+					// 		}
+                    //
+					// 		if (! foundVocab) {
+					// 			json.vocabulary.splice(v, 1);
+					// 			updateJSON = true;
+					// 		}
+					// 	}
+					// }
+
+					if (updateJSON) {
 						fs.writeFile(path.join(__dirname, templateName, fileName),
 							JSON.stringify(json, null, 4), function(writeError) {
 								if (writeError)
@@ -75,7 +193,7 @@ function render(templateName, fileName) {
 				}
 
 				json.type = templateName;
-				json.id = path.basename(fileName, '.json');
+				json.id = fileId;
 
 				// Refer to build and data cache
 				json.build = build;
